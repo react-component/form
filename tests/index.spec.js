25d9bfe8120b15de6044884b01f62058c5cd3ac9
yiminghe <yiminghe@gmail.com>
2016-02-17 11:34:43 +0800

    optimize tests

--- tests/index.spec.js
+++
@@ -1,8 +0,0 @@
-import './overview.spec';
-import './async.spec';
-import './initialValue.spec';
-import './normalize';
-import './map.spec';
-import './forceValidate.spec';
-import './message.spec';
-import './refComponent.spec';
