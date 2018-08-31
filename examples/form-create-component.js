18db4b9775cc43bbcd1a0442e5375aebd63e3b41
吴俊骁 <mushan0x0@gmail.com>
2018-08-31 12:26:49 +0800

    update: 添加setRef用于设置form

--- examples/form-create-component.js
+++ examples/form-create-component.js
@@ -5,44 +5,38 @@ import { FormCreate } from '../src';
 class Page extends React.Component {
   componentDidMount() {
     setTimeout(() => {
-      this.formA.setFieldsValue({ field_a: 'this is field_a.' });
-      this.formB.setFieldsValue({ field_b: 'this is field_b.' });
+      this.formA.current.setFieldsValue({ field_a: 'this is field_a.' });
+      this.formB.current.setFieldsValue({ field_b: 'this is field_b.' });
     }, 2000);
   }
   resetAll = () => {
-    this.formA.resetFields();
-    this.formB.resetFields();
+    this.formA.current.resetFields();
+    this.formB.current.resetFields();
   }
 
-  formA;
-  formB;
+  formA = React.createRef();
+  formB = React.createRef();
 
   render() {
     return (
       <div>
-        <FormCreate>
-          {form => {
-            this.formA = form;
-            return (
-              <div>
-                Group A
-                <input {...form.getFieldProps('field_a')}/>
-                {/* more fields or complex logic */}
-              </div>
-            );
-          }}
+        <FormCreate setRef={this.formA}>
+          {({ getFieldProps }) => (
+            <div>
+              Group A
+              <input {...getFieldProps('field_a')}/>
+              {/* more fields or complex logic */}
+            </div>
+          )}
         </FormCreate>
-        <FormCreate>
-          {form => {
-            this.formB = form;
-            return (
-              <div>
-                Group B
-                <input {...form.getFieldProps('field_b')}/>
-                {/* more fields or complex logic */}
-              </div>
-            );
-          }}
+        <FormCreate setRef={this.formB}>
+          {({ getFieldProps }) => (
+            <div>
+              Group B
+              <input {...getFieldProps('field_b')}/>
+              {/* more fields or complex logic */}
+            </div>
+          )}
         </FormCreate>
 
         <button onClick={this.resetAll}>Reset All</button>
--- src/createFormComponent.js
+++ src/createFormComponent.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import createDOMForm from './createDOMForm';
 
-const FormCreate = ({ children, ...options }) => {
-  const WrappedComponent = createDOMForm(options)(({ form }) => children(form));
+const FormCreate = ({ children, setRef, ...options }) => {
+  const WrappedComponent = createDOMForm(options)(({ form }) => {
+    if (setRef) {
+      if (typeof setRef === 'function') {
+        setRef(form);
+      } else if (setRef.current === null) {
+        setRef.current = form;
+      }
+    }
+    return children(form);
+  });
   return <WrappedComponent />;
 };
 
