d243e842f6830f68ab07cbe95c1336dddf64fe99
吴俊骁 <mushan0x0@gmail.com>
2018-09-01 19:48:36 +0800

    update: 添加options types

--- src/createFormComponent.js
+++ src/createFormComponent.js
@@ -5,7 +5,17 @@ import PropTypes from 'prop-types';
 class FormCreate extends React.Component {
   static propTypes = {
     children: PropTypes.func,
+    validateMessages: PropTypes.object,
+    onFieldsChange: PropTypes.func,
+    onValuesChange: PropTypes.func,
+    mapProps: PropTypes.func,
+    mapPropsToFields: PropTypes.func,
+    fieldNameProp: PropTypes.string,
+    fieldMetaProp: PropTypes.string,
+    fieldDataProp: PropTypes.string,
+    withRef: PropTypes.bool,
   };
+
   render() {
     const { children, ...options } = this.props;
     const WrappedComponent = createDOMForm(options)(({ form }) => {
